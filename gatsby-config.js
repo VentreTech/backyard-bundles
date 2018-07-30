f4382dab0eec1d30c6193437e9cf962d139e3e53
Gary Ditsch <garyditsch@gmail.com>
2018-07-30 15:02:35 -0400

    adding packages and plugins for gatsby to create webmanifest and service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,5 +15,18 @@ module.exports = {
     'gatsby-transformer-remark',
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: "GatsbyJS",
+    //     short_name: "GatsbyJS",
+    //     start_url: "/",
+    //     background_color: "#f7f0eb",
+    //     theme_color: "#a2466c",
+    //     display: "minimal-ui",
+    //     icon: "src/images/icon.png", // This path is relative to the root of the site.
+    //   },
+    // },
+    `gatsby-plugin-offline`
   ],
 }
